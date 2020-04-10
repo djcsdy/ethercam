76c0058071b23e85f28327f2c29c1cccc2948cad
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-04-10 16:32:01 +0100

    Navigate to CameraActivity when user presses start button.

--- app/src/main/java/net/noiseinstitute/ethercam/MainActivity.kt
+++ app/src/main/java/net/noiseinstitute/ethercam/MainActivity.kt
@@ -1,6 +1,9 @@
 package net.noiseinstitute.ethercam
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import net.noiseinstitute.ethercam.http_server.HttpServer
 
@@ -8,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            startActivity(Intent(this, CameraActivity::class.java))
+        }
 
         HttpServer().start()
     }
