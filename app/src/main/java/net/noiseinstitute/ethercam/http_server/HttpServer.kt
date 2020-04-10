ae6133db7ad89ea48c1dfa7bd842f5a1843474ff
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-04-10 15:35:49 +0100

    Move web server into its own file.

---
+++ app/src/main/java/net/noiseinstitute/ethercam/http_server/HttpServer.kt
@@ -0,0 +1,57 @@
+package net.noiseinstitute.ethercam.http_server
+
+import android.util.Log
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.ServerSocket
+
+private const val port = 8080
+
+class HttpServer {
+    fun start() {
+        Thread(Runnable {
+            while (true) {
+                try {
+                    ServerSocket(port).use { socket ->
+                        while (true) {
+                            socket.accept().use { connection ->
+                                val inputReader = BufferedReader(
+                                    InputStreamReader(
+                                        connection.getInputStream()
+                                    )
+                                )
+
+                                val request = inputReader.readLine()
+                                while (inputReader.readLine() != "") {
+                                    // Do nothing
+                                }
+
+                                BufferedWriter(
+                                    OutputStreamWriter(
+                                        connection.getOutputStream(), "UTF-8"
+                                    )
+                                ).use { outputWriter ->
+                                    if (request == null || !request.startsWith("GET ")
+                                        || !(request.endsWith("HTTP/1.0")
+                                                || request.endsWith("HTTP/1.1"))
+                                    ) {
+                                        outputWriter.write("HTTP/1.0 400 Bad Request\r\n\r\n")
+                                    } else {
+                                        outputWriter.write("HTTP/1.0 200 OK\r\n")
+                                        outputWriter.write("Content-Type: text/plain\r\n")
+                                        outputWriter.write("\r\n")
+                                        outputWriter.write("Hello, World!")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.e("Ethercam", "Socket error", e)
+                }
+            }
+        }).start()
+    }
+}
