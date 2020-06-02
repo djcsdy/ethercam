35f7187d9be6726b822fb9fe7ec4b143c7c04766
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-06-02 04:00:20 +0100

    Fix camera preview not displaying except immediately after permission is granted.

--- app/src/main/java/net/noiseinstitute/ethercam/camera/Camera.kt
+++ app/src/main/java/net/noiseinstitute/ethercam/camera/Camera.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.hardware.display.DisplayManager
 import android.os.Handler
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
@@ -16,7 +17,7 @@ internal class Camera(
 ) {
     private var camera: android.hardware.Camera? = null
     private var layout: ConstraintLayout? = null
-    private var surfaceView: SurfaceView? = null
+    private var previewSurfaceView: SurfaceView? = null
 
     private val displayListener = object : DisplayManager.DisplayListener {
         override fun onDisplayChanged(displayId: Int) {
@@ -32,11 +33,23 @@ internal class Camera(
         }
     }
 
+    private val previewSurfaceCallback = object : SurfaceHolder.Callback {
+        override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        }
+
+        override fun surfaceCreated(holder: SurfaceHolder?) {
+            startPreview()
+        }
+    }
+
     fun start(layout: ConstraintLayout, surfaceView: SurfaceView) {
         stop()
 
         this.layout = layout
-        this.surfaceView = surfaceView
+        this.previewSurfaceView = surfaceView
 
         val displayManager = ActivityCompat.getSystemService(activity, DisplayManager::class.java)
         displayManager?.registerDisplayListener(displayListener, Handler())
@@ -45,12 +58,13 @@ internal class Camera(
     }
 
     fun stop() {
-        layout = null
-        surfaceView = null
+        stopCamera()
 
         val displayManager = ActivityCompat.getSystemService(activity, DisplayManager::class.java)
         displayManager?.unregisterDisplayListener(displayListener)
-        stopCamera()
+
+        layout = null
+        previewSurfaceView = null
     }
 
     private fun startCamera() {
@@ -72,15 +86,15 @@ internal class Camera(
         }
 
         updateOrientation()
+        camera?.let { setFocusMode(it) }
+        startPreview()
 
-        camera?.let { camera ->
-            setFocusMode(camera)
-            surfaceView?.let { camera.setPreviewDisplay(it.holder) }
-            camera.startPreview()
-        }
+        previewSurfaceView?.holder?.addCallback(previewSurfaceCallback)
     }
 
     private fun stopCamera() {
+        previewSurfaceView?.holder?.removeCallback(previewSurfaceCallback)
+
         camera?.release()
         camera = null
     }
@@ -90,12 +104,19 @@ internal class Camera(
             val displayOrientation = calculateDisplayOrientation(activity)
             camera.setDisplayOrientation(displayOrientation)
             val previewSize = camera.parameters.previewSize
-            layout?.let {layout ->
-                surfaceView?.let {surfaceView ->
+            layout?.let { layout ->
+                previewSurfaceView?.let { surfaceView ->
                     val aspectRatio = calculateAspectRatio(previewSize, displayOrientation)
                     setAspectRatio(layout, surfaceView, aspectRatio)
                 }
             }
+        }
+    }
+
+    private fun startPreview() {
+        camera?.let { camera ->
+            previewSurfaceView?.let { camera.setPreviewDisplay(it.holder) }
+            camera.startPreview()
         }
     }
 }
