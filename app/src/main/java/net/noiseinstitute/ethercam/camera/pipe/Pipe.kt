4ec0fde5b293db8e628b6064cd022dc18a54ef1b
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-06-30 02:54:14 +0100

    Fix Pipe never accepting connection.

--- app/src/main/java/net/noiseinstitute/ethercam/camera/pipe/Pipe.kt
+++ app/src/main/java/net/noiseinstitute/ethercam/camera/pipe/Pipe.kt
@@ -1,50 +1,60 @@
 package net.noiseinstitute.ethercam.camera.pipe
 
 import android.content.Context
+import android.net.LocalServerSocket
 import android.net.LocalSocket
 import android.net.LocalSocketAddress
 import androidx.annotation.AnyThread
 import androidx.annotation.UiThread
 import java.io.Closeable
+import java.io.IOException
 import java.util.*
 import kotlin.collections.HashSet
 
 internal class Pipe private constructor(
-    private val sender: LocalSocket,
-    private val receiver: LocalSocket,
+    private val server: LocalSocket,
+    private val client: LocalSocket,
     private val sinks: MutableSet<Sink>
-): Closeable {
-    val fileDescriptor = sender.fileDescriptor
+) : Closeable {
+    val fileDescriptor = client.fileDescriptor
 
     companion object {
         @UiThread
         fun open(context: Context): Pipe {
-            val name = context.getFileStreamPath("VideoPipe" + UUID.randomUUID()).absolutePath
+            val name = context
+                .getFileStreamPath("Pipe-" + UUID.randomUUID())
+                .absolutePath
             val address = LocalSocketAddress(name, LocalSocketAddress.Namespace.FILESYSTEM)
+            val server = LocalSocket(LocalSocket.SOCKET_STREAM)
+            server.bind(address)
+            val serverWrapper = LocalServerSocket(server.fileDescriptor)
 
-            val receiver = LocalSocket(LocalSocket.SOCKET_STREAM)
-            receiver.bind(address)
-
-            val sender = LocalSocket(LocalSocket.SOCKET_STREAM)
-            sender.connect(address)
+            val client = LocalSocket(LocalSocket.SOCKET_STREAM)
+            client.connect(address)
 
             val sinks = Collections.synchronizedSet(HashSet<Sink>())
 
             Thread {
-                val inputStream = receiver.inputStream.buffered()
+                serverWrapper.use { server ->
+                    val receiver = server.accept()
+                    val inputStream = receiver.inputStream
+                    val buffer = ByteArray(188)
 
-                while (!receiver.isClosed) {
-                    val bytes = inputStream.readBytes()
-                    val iterator = sinks.iterator()
-                    for (sink in iterator) {
-                        if (!sink.receive(bytes)) {
-                            iterator.remove()
+                    var byteCount = inputStream.read(buffer)
+                    while (byteCount != -1) {
+                        val bytes = buffer.sliceArray(0 until byteCount)
+                        val iterator = sinks.iterator()
+                        for (sink in iterator) {
+                            if (!sink.receive(bytes)) {
+                                iterator.remove()
+                            }
                         }
+                        byteCount = inputStream.read(buffer)
                     }
                 }
             }.start()
 
-            return Pipe(sender, receiver, sinks)
+            return Pipe(server, client, sinks)
         }
     }
 
@@ -55,8 +65,24 @@ internal class Pipe private constructor(
 
     @UiThread
     override fun close() {
-        receiver.use {
-            sender.close()
+        server.use {
+            try {
+                server.shutdownInput()
+            } catch (ignore: IOException) {
+            }
+            try {
+                server.shutdownOutput()
+            } catch (ignore: IOException) {
+            }
+            try {
+                client.shutdownOutput()
+            } catch (ignore: IOException) {
+            }
+            try {
+                client.shutdownInput()
+            } catch (ignore: IOException) {
+            }
+            client.close()
         }
     }
 }
